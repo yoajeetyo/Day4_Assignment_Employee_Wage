3d576278de15d76f8d9c41392e899cd1152204bb
yoajeetyo <sajeetkumar40@gmail.com>
2021-08-22 05:46:51 +0530

    Added : Wage calculation for 20 days in a month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,10 +2,13 @@ public class EmployeeWage {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
+	public static final int TOTAL_DAYS=20;
 
 	public static void main(String[] args) {
 		int empHrs=0;
 		int empWage=0;
+		int totalWage=0;
+		for (int day=0;day<20;day++){
 		int employeeCheck =(int) Math.floor(Math.random() * 10) % 3;
 		switch (employeeCheck) {
 			case IS_FULL_TIME :
@@ -19,5 +22,8 @@ public class EmployeeWage {
 		}
 		empWage=empHrs*EMP_RATE_PER_HOUR;
 		System.out.println("Emp Wage is : "+empWage );
+		totalWage+=empWage;
+		}
+		System.out.println("Total Wage is : "+totalWage);
 	}
 }
