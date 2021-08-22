e6e51c420791c5cada3544ad2a30e3ca50cac571
yoajeetyo <sajeetkumar40@gmail.com>
2021-08-22 05:37:43 +0530

    Added : Wage calculation using switch case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,22 +1,22 @@
 public class EmployeeWage {
-	public static int IS_FULL_TIME = 1;
-	public static int IS_PART_TIME=2;
-	public static int EMP_RATE_PER_HOUR=20;
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=20;
 
 	public static void main(String[] args) {
 		int empHrs=0;
 		int empWage=0;
-		double employeeCheck = Math.floor(Math.random() * 10) % 3;
-		if (employeeCheck == IS_FULL_TIME) {
-			empHrs=8;
+		int employeeCheck =(int) Math.floor(Math.random() * 10) % 3;
+		switch (employeeCheck) {
+			case IS_FULL_TIME :
+				empHrs=8;
+				break;
+			case IS_PART_TIME :
+				empHrs=4;
+				break;
+			default :
+				empHrs=0;
 		}
-		else if (employeeCheck == IS_PART_TIME){
-			empHrs=4;
-		}
-		else {
-			empHrs=0;
-		}
-
 		empWage=empHrs*EMP_RATE_PER_HOUR;
 		System.out.println("Emp Wage is : "+empWage );
 	}
