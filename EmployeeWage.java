1e42fb1813fe944829ba3e295fb3e885ce0af230
yoajeetyo <sajeetkumar40@gmail.com>
2021-08-22 05:56:30 +0530

    Modified : Calculation of employee wage either complition of 20 days or 100 hours for a month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,12 +3,15 @@ public class EmployeeWage {
 	public static final int IS_PART_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
 	public static final int TOTAL_DAYS=20;
+	public static final int MAX_HRS_IN_A_MONTH=100;
 
 	public static void main(String[] args) {
-		int empHrs=0;
-		int empWage=0;
 		int totalWage=0;
-		for (int day=0;day<20;day++){
+		int totalWorkingDays=0;
+		int totalEmpHrs=0;
+		while (totalEmpHrs<=MAX_HRS_IN_A_MONTH && totalWorkingDays<TOTAL_DAYS){
+			int empHrs=0;
+			totalWorkingDays++;
 		int employeeCheck =(int) Math.floor(Math.random() * 10) % 3;
 		switch (employeeCheck) {
 			case IS_FULL_TIME :
@@ -20,10 +23,12 @@ public class EmployeeWage {
 			default :
 				empHrs=0;
 		}
-		empWage=empHrs*EMP_RATE_PER_HOUR;
+		totalEmpHrs +=empHrs;
+		int empWage=empHrs*EMP_RATE_PER_HOUR;
 		System.out.println("Emp Wage is : "+empWage );
 		totalWage+=empWage;
 		}
+		System.out.println("Total working hours in a month is : "+totalEmpHrs);
 		System.out.println("Total Wage is : "+totalWage);
 	}
 }
